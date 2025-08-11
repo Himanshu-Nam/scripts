86929b124aa05cd255a24a46da6000a3e17e7655
Ubuntu <ubuntu@ip-172-31-24-225.eu-west-1.compute.internal>
2025-08-11 17:41:42 +0000

    added 3 idiots scripts

---
+++ 3idots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#bash.sh him nam ok
+echo "First arg : "$0
+echo " second arg : "$1
+echo "Third arg :"$2
+echo "Fourth arg ;"$3
+echo "all arg complete which is $@"
+echo "Total no . of arg is $#"
+hero="Himanshu"
+role="Devops engineer"
+echo "name of my sucessfull employee is " $hero
+echo "role is " $role
+# shell / enviroment variable
+echo "Current login user is $USER"
+#read -p "Enter your name : " fullname
+#echo "your name is : " $fullname
+
